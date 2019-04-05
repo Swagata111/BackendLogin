ffd37511636a061425b32bc1ad2bc4767a010db4
Swagata Kundu <swagatakundu1995@gmail.com>
2019-04-05 17:30:46 +0530

    Add files via upload

---
+++ Lazy-Days-Main/src/main/java/com/capgemini/lazyDays/LazyDaysMainApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.lazyDays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class LazyDaysMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyDaysMainApplication.class, args);
+	}
+}
